a8e9dc484d8c6729b36727acbe9f398ebd26a68b
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2026-02-12 23:46:47 +0530

    controller and  model folder

---
+++ controllers/auth.controller.js
@@ -0,0 +1,2 @@
+import express from 'express'
+import userModel from '../src/models/user.model'
--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
-
+import router from './routes/auth.routes';
 
 
 const app=express();
 
+app.use("/api/auth",router)
 
-export default app
+
+
+
+
+export default app;
